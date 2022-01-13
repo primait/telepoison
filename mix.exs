20061b57578b971f06d9af818b2b92b5a771e77a
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2022-01-13 16:58:35 +0100

    Replace Mix.Config with Config

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Telepoison.MixProject do
     [
       app: :telepoison,
       version: @version,
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs(),
