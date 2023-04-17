5b99ab77dbfb675db525d3b0b5c3ee65a50d507c
Cristiano Piemontese <cristiano.piemontese@prima.it>
2023-04-17 14:34:55 +0200

    add validation tests

--- lib/configuration.ex
+++ lib/configuration.ex
@@ -8,16 +8,20 @@ defmodule Telepoison.Configuration do
     route_inference_fn = get(:infer_route)
 
     errors =
-      if is_function(route_inference_fn, 1),
-        do: errors,
-        else: ["The configured :infer_route keyword option value must be a function with an arity of 1" | errors]
+      add_if(
+        not is_function(route_inference_fn, 1),
+        errors,
+        ["The configured :infer_route keyword option value must be a function with an arity of 1" | errors]
+      )
 
     ot_attributes = get(:ot_attributes)
 
     errors =
-      if is_list(ot_attributes),
-        do: errors,
-        else: ["The configured :ot_attributes option must be a [{key, value}] list"]
+      add_if(
+        not is_list(ot_attributes),
+        errors,
+        ["The configured :ot_attributes option must be a [{key, value}] list"]
+      )
 
     case errors do
       [] -> :ok
@@ -34,4 +38,8 @@ defmodule Telepoison.Configuration do
   def get(:infer_route), do: Application.get_env(:telepoison, :infer_route, &Telepoison.URI.infer_route_from_request/1)
 
   def get(:ot_attributes), do: Application.get_env(:telepoison, :ot_attributes, [])
+
+  defp add_if(condition, list, value)
+  defp add_if(true, list, value), do: [value | list]
+  defp add_if(false, list, _), do: list
 end
--- test/configuration_test.exs
+++ test/configuration_test.exs
@@ -1,5 +1,7 @@
 defmodule ConfigurationTest do
   alias Telepoison.Configuration
+  alias Telepoison.TestApi
+
   use ExUnit.Case
 
   doctest Telepoison
@@ -7,5 +9,38 @@ defmodule ConfigurationTest do
   test "it does not crash on `get` if it has not been setup" do
     assert Configuration.get(:infer_route)
     assert Configuration.get(:ot_attributes)
+  end
+
+  test "if :ot_attributes are not a list it returns error" do
+    TestApi.set_env(:ot_attributes, {:key, :value})
+
+    {:error, errors} = Configuration.validate()
+    assert length(errors) == 1
+  end
+
+  test "if :ot_attributes are a list it returns ok" do
+    TestApi.set_env(:ot_attributes, [{:key, :value}])
+
+    assert Configuration.validate() == :ok
+  end
+
+  test "if :infer_route is not a function it returns error" do
+    TestApi.set_env(:infer_route, :not_a_function)
+
+    {:error, errors} = Configuration.validate()
+    assert length(errors) == 1
+  end
+
+  test "if :infer_route is a function of arity other than 1 it returns error" do
+    TestApi.set_env(:infer_route, fn x, y, z -> {x, y, z} end)
+
+    {:error, errors} = Configuration.validate()
+    assert length(errors) == 1
+  end
+
+  test "if :infer_route is a function of arity 1 it returns ok" do
+    TestApi.set_env(:infer_route, fn x -> x end)
+
+    Configuration.validate() == :ok
   end
 end
