017a389b3f34f046d2efe29457f49685cfb9a6d1
Cristiano Piemontese <cristiano.piemontese@prima.it>
2023-04-17 14:11:37 +0200

    fix compile error

--- test/support/test_api.ex
+++ test/support/test_api.ex
@@ -1,13 +1,13 @@
 defmodule Telepoison.TestApi do
   @moduledoc false
 
-  use ExUnit.Case
+  alias ExUnit.Callbacks
 
   def set_env(key, value) do
     env_to_restore = Application.get_env(:telepoison, key, :unset)
     Application.put_env(:telepoison, key, value)
 
-    on_exit(fn ->
+    Callbacks.on_exit(fn ->
       case env_to_restore do
         :unset -> Application.delete_env(:telepoison, key)
         _ -> Application.put_env(:telepoison, key, env_to_restore)
